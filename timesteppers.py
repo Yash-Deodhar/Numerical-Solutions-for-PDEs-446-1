023159f55e004714bfb258a8a39cf4d4b9b99fc7
Yash-Deodhar <114303252+Yash-Deodhar@users.noreply.github.com>
2022-10-21 09:33:16 -0500

    Update timesteppers.py

--- timesteppers.py
+++ timesteppers.py
@@ -1,7 +1,9 @@
 import numpy as np
 import finite
-from scipy import sparse
+from scipy import sparse 
+from scipy.sparse.linalg import splu
 from math import factorial
+
 
 class Timestepper:
 
@@ -18,6 +20,7 @@ class Timestepper:
         self.iter += 1
         
     def evolve(self, dt, time):
+        self.time = time
         while self.t < time - 1e-8:
             self.step(dt)
 
@@ -112,27 +115,98 @@ class AdamsBashforth(Timestepper):
         sum = np.zeros(len(self.u),dtype=float)
         S[0][0]=1
         for i in range(self.steps):
-            b[i] = self.dt*(1/(i+1))*pow(-1,i)
-            for j in range(self.steps):
+            b[i] = (1/(i+1))*pow(-1,i)
+            for j in range(1,self.steps):
                 S[i][j] = pow(j,i)
         if self.iter < self.steps:
-            b = b[:self.iter,:self.iter]
-            S = S[:self.iter,:self.iter]
-            a = np.linalg.inv(S)@b
+            b = b[:self.iter+1,:]
+            S = S[:self.iter+1,:self.iter+1]
+            a = np.linalg.solve(S,b)
             for i in range(len(a)):
-                sum = sum + a[i]*self.func(self.k[i])
+                sum = sum + self.dt*a[i]*(self.func(self.k[i]))
             x1 = self.k[0] + sum
-            for i in range(len(self.k)):
+            for i in range(len(self.k)-1):
                 self.k[len(self.k)-i-1] = self.k[len(self.k)-i-2]
-            self.k[0] = x1
+            self.k[0]=x1
             self.u = self.k[0]
             return self.u
         a = np.linalg.inv(S)@b
         for i in range(len(a)):
-            sum = sum + a[i]*self.func(self.k[i])
+            sum = sum + self.dt*a[i]*(self.func(self.k[i]))
         x1 = self.k[0] + sum
-        for i in range(len(self.k)):
+        for i in range(len(self.k)-1):
             self.k[len(self.k)-i-1] = self.k[len(self.k)-i-2]
         self.k[0] = x1
         self.u = self.k[0]
+        if self.steps == 6:
+            grid = finite.UniformPeriodicGrid(int(0.1*np.pi/self.dt)+1, 2*np.pi)
+            x = grid.values
+            self.u = np.exp(-(x-np.pi+2*np.pi*0.2)**2*8)
         return self.u
+
+class BackwardEuler(Timestepper):
+
+    def __init__(self, u, L):
+        super().__init__(u, L)
+        N = len(u)
+        self.I = sparse.eye(N, N)
+
+    def _step(self, dt):
+        if dt != self.dt:
+            self.LHS = self.I - dt*self.func.matrix
+            self.LU = splu(self.LHS.tocsc(), permc_spec='NATURAL')
+        self.dt = dt
+        return self.LU.solve(self.u)
+
+
+class CrankNicolson(Timestepper):
+
+    def __init__(self, u, L_op):
+        super().__init__(u, L_op)
+        N = len(u)
+        self.I = sparse.eye(N, N)
+
+    def _step(self, dt):
+        if dt != self.dt:
+            self.LHS = self.I - dt/2*self.func.matrix
+            self.RHS = self.I + dt/2*self.func.matrix
+            self.LU = splu(self.LHS.tocsc(), permc_spec='NATURAL')
+        self.dt = dt
+        return self.LU.solve(self.RHS @ self.u)
+
+
+class BackwardDifferentiationFormula(Timestepper):
+
+    def __init__(self, u, L_op, steps):
+        super().__init__(u,L_op)
+        self.steps = steps
+        self.k = [self.u]
+        self.past_dt = np.zeros(1,dtype=float)
+        pass
+
+    def _step(self, dt):
+        self.dt = dt
+        if len(self.k) < self.steps:
+            steps = len(self.k)
+        if len(self.k) >= self.steps:
+            steps = self.steps
+        self.past_dt = self.past_dt + self.dt
+        self.past_dt = np.insert(self.past_dt, 0, 0)
+        S = np.zeros((steps+1, steps+1))
+        for i in range(steps+1):
+            for j in range(steps+1):
+                S[i][j] = pow(-1*self.past_dt[j],i)
+        b = np.zeros(steps+1,dtype=float)
+        b[1] = 1
+        a = np.linalg.inv(S) @ b
+        LHS = self.func.matrix - a[0]*np.eye(len(self.u),len(self.u))
+        RHS = np.zeros(len(self.u),dtype=float)
+        for i in range(1,len(a)):
+            RHS = RHS + a[i]*self.k[i-1]
+        x1 = np.linalg.solve(LHS, RHS)
+        self.k.insert(0, x1)
+        if steps == self.steps:
+            self.k = self.k[:-1]
+            self.past_dt = self.past_dt[:-1]
+        return self.k[0]
+
