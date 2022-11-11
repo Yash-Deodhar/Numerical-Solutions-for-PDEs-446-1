48a26b7e9b8d6aac20a6c18d7c99c3d807339ffb
Yash-Deodhar <114303252+Yash-Deodhar@users.noreply.github.com>
2022-11-11 08:19:37 -0600

    Update equations.py

--- equations.py
+++ equations.py
@@ -1,6 +1,5 @@
 from scipy import sparse
-from sympy import SparseMatrix
-from timesteppers import StateVector, CrankNicolson, RK22, Timestepper
+from timesteppers import StateVector, CrankNicolson, RK22
 import finite
 import numpy as np
 
@@ -75,8 +74,8 @@ class Burger_DiffusionX_Eqn:
         diff_operator = -D*dx2.matrix
 
         L00 = diff_operator
-        L01 = I
-        L10 = I
+        L01 = Z
+        L10 = Z
         L11 = diff_operator
         self.L = sparse.bmat([[L00, L01],
                               [L10, L11]])
@@ -84,6 +83,7 @@ class Burger_DiffusionX_Eqn:
 class Burger_DiffusionY_Eqn:
     
     def __init__(self, u, v, D, dy2):
+
         self.X = StateVector([u, v], axis=1)
         N = len(u)
 
@@ -100,8 +100,10 @@ class Burger_DiffusionY_Eqn:
         diff_operator = -D*dy2.matrix
 
         L00 = diff_operator
-        L01 = I
-        L10 = I
+        L01 = Z
+
+        
+        L10 = Z
         L11 = diff_operator
         self.L = sparse.bmat([[L00, L01],
                               [L10, L11]])
@@ -238,4 +240,163 @@ class ReactionDiffusion:
 
         
         self.F = lambda X: X.data*(c_target-X.data) 
+
+class DiffusionxBC:
     
+    def __init__(self, c, D, d2x,dx):
+        self.X = StateVector([c], axis=0)
+        N = c.shape[0]
+        M = sparse.eye(N, N)        
+        M = M.tocsr()
+        M[0,:] = 0
+        M[-1,:] = 0
+        M.eliminate_zeros()
+        self.M = M        
+        L = -D*d2x.matrix
+        L = L.tocsr()
+        L[0,:] = 0
+        L[-1,:] = dx.matrix[-1,:]
+        L[0, 0] = 1
+        L.eliminate_zeros()
+        self.L = L
+
+class DiffusionyBC:
+    
+    def __init__(self, c, D, d2y):
+        self.X = StateVector([c], axis=1)
+        N = c.shape[1]
+        M = sparse.eye(N, N)
+        self.M = M        
+        L = -D*d2y.matrix
+        self.L = L
+
+class DiffusionBC:
+    def __init__(self,c, D, spatial_order, domain):
+        self.c = c
+        self.D = D
+        self.spatial_order = spatial_order
+        self.domain = domain
+        self.dx2 = finite.DifferenceUniformGrid(2, self.spatial_order, self.domain.grids[0], 0)
+        self.dx = finite.DifferenceUniformGrid(1,self.spatial_order, self.domain.grids[0], 0)
+        self.dy2 = finite.DifferenceUniformGrid(2, self.spatial_order, self.domain.grids[1], 1)
+        self.diffy = DiffusionyBC(self.c,self.D,self.dy2)
+        self.diffx = DiffusionxBC(self.c,self.D,self.dx2,self.dx) 
+        self.t = 0
+        self.iter = 0
+        self.ts_y = CrankNicolson(self.diffy, 1)
+        self.ts_x = CrankNicolson(self.diffx, 0)
+    
+    def step(self,dt):
+        self.ts_y.step(dt/2)
+        self.ts_x.step(dt/1)
+        self.ts_y.step(dt/2)
+        self.t += dt
+        self.iter += 1
+        pass
+
+class Advection_EqnX:
+    
+    def __init__(self, u, v, dx, dy):
+        self.X = StateVector([u, v])
+        n = len(u)
+        def f(K):
+            u1 = K.data[0:n, :]
+            v1 = K.data[:-n, :]
+            u1out = - u1 * (dx @ u1) - v1 * (dy @ u1)
+            u2out = - u1 * (dx @ v1) - v1 * (dy @ v1)
+            return np.concatenate((u1out, u2out))    
+        self.F = f
+
+class ViscousBurgers2D:
+
+    def __init__(self, u, v, nu, spatial_order, domain):
+        self.iter = 0
+        self.t = 0
+
+        dx2 = finite.DifferenceUniformGrid(2, spatial_order, domain.grids[0], 0)
+        dy2 = finite.DifferenceUniformGrid(2, spatial_order, domain.grids[1], 1)
+        dy = finite.DifferenceUniformGrid(1, spatial_order, domain.grids[1], 1)
+        dx = finite.DifferenceUniformGrid(1, spatial_order, domain.grids[0], 0)
+
+        DiffX_Eqn = Burger_DiffusionX_Eqn(u, v, nu, dx2)
+        DiffY_Eqn = Burger_DiffusionY_Eqn(u, v, nu, dy2)
+        self.DiffusionX = CrankNicolson(DiffX_Eqn, 0)
+        self.DiffusionY = CrankNicolson(DiffY_Eqn, 1)
+        self.AdvectionX = RK22(Advection_EqnX(u, v, dx, dy))
+
+    def step(self, dt):
+        self.DiffusionX.step(dt/4)
+        self.DiffusionY.step(dt/2)
+        self.DiffusionX.step(dt/4)
+
+        self.AdvectionX.step(dt)
+
+        self.DiffusionX.step(dt/4)
+        self.DiffusionY.step(dt/2)
+        self.DiffusionX.step(dt/4)
+
+        self.iter += 1
+        self.t += dt
+
+class Wave2DBC:
+    def __int__(self, u ,v ,p ,spatial_order ,domain):
+        self.u = u
+        self.v = v
+        self.p = p
+        self.spatial_order = spatial_order
+        self.domain = domain
+        N = u.shape()
+        self.dx = finite.DifferenceUniformGrid(1,self.spatial_order,domain.grids[0])
+        self.dy = finite.DifferenceUniformGrid(1,self.spatial_order,domain.grids[1])
+        self.M = sparse.eye(N,N)
+        self.L = sparse.eye(N,N)*(self.dx.matrix + self.dy.matrix)
+        self.AdvectionX = RK22(Advection_EqnX(u, v, self.dx, self.dy))
+
+    def step(self,dt):
+        self.dt = dt
+        pass
+
+class Wave2DEvolve:
+    
+    def __init__(self, p, U, D):
+        self.X = StateVector([p, U])
+        n = len(p)
+
+        def f(K):
+            # K.scatter()
+            p = K.data[:n, :]
+            U = K.data[-n:, :]
+
+            pout = D @ U
+            Uout = D @ p
+
+            return np.concatenate((pout, Uout))
+            
+        self.F = f
+
+class Wave2DBC:
+
+    def __init__(self, u, v, p, spatial_order, domain):
+        self.iter = 0
+        self.t = 0
+        dy = finite.DifferenceUniformGrid(1, spatial_order, domain.grids[1], 1)
+        dx = finite.DifferenceUniformGrid(1, spatial_order, domain.grids[0], 0)
+
+        evolveX_eqn = Wave2DEvolve(p, u, dx)
+        evolveY_eqn = Wave2DEvolve(p, v, dy)
+        self.evolveX = RK22(evolveX_eqn)
+        self.evolveY = RK22(evolveY_eqn)
+
+    def BC(self, X):
+        n = len(X.variables[0])
+        u = X.data[-n:, :]
+        u[:, -1] = 0
+        u[:, 0] = 0
+
+    def step(self, dt):
+        self.evolveX.step(dt/2)
+        self.evolveY.step(dt/2)
+
+        self.iter += 1
+        self.t += dt
+
